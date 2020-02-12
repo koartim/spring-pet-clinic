518a952ca2fb70bec416a5b262a7772d3d2ace5f
Timothy Koar <koartimothy@gmail.com>
2020-02-12 17:02:46 -0500

    added owners controller and view closes #16

---
+++ pet-clinic-web/src/main/java/guru/springframework/springpetclinic/controllers/PetController.java
@@ -0,0 +1,12 @@
+package guru.springframework.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PetController {
+
+    public String listPets() {
+        return
+    }
+
+}
