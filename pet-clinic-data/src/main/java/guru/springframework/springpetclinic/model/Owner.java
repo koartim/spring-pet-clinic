ca07e537a895769572a6b519158f692fd58e2e4f
Timothy Koar <koartimothy@gmail.com>
2020-02-15 10:27:43 -0500

    added pet pettype and visit entities closes #26

--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/model/Owner.java
@@ -1,6 +1,16 @@
 package guru.springframework.springpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
-    public Owner() {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
