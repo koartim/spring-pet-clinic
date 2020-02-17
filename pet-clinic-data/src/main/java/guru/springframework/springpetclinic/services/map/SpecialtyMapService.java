f55d2cb6c5b75ef63d0f396aba1b608beabc712b
Timothy Koar <koartimothy@gmail.com>
2020-02-17 09:19:08 -0500

    added specialty map service interface and impl closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,36 @@
+package guru.springframework.springpetclinic.services.map;
+
+import guru.springframework.springpetclinic.model.Specialty;
+import guru.springframework.springpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
