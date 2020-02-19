c67037ae20326f19e9a1132df63c1374176f14bb
Timothy Koar <koartimothy@gmail.com>
2020-02-19 10:29:42 -0500

    Converted vets to JPA entities closes #37

--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/model/Vet.java
@@ -1,10 +1,18 @@
 package guru.springframework.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    // default many relationships is lazily fetched, meaning it won't load until you ask for it, setting it to eager means that a vets specialties
+    // will load when you ask for a vet, specialties would be null if you do not add this
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
