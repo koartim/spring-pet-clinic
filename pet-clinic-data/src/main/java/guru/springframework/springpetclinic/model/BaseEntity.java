c938cd75a49e0cd8cfb12c2483f1c6f505140fcd
Timothy Koar <koartimothy@gmail.com>
2020-03-10 15:17:37 -0400

    fix Owner display

--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/model/BaseEntity.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
 // hey JPA we are going to have our models inherit from this class but we don't need it in the database
 @Getter
